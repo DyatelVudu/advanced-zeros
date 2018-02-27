166e7a7dfd07fdcddeb9e93905da43f3005d17f1
Ilkya Kutko <36884482+DyatelVudu@users.noreply.github.com>
2018-02-27 16:55:19 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	let a = [];
+	for(let i = 2;i <= base; i++) {
+		if(base % i == 0) {
+            if(a[i] !== undefined){
+                a[i] += 1;
+            } else a[i] = 1;
+
+			base = base / i;
+			i--;
+		}
+	}
+
+    let b = [];
+    a.forEach((el, index)=>{
+        if(el !== undefined)
+            b[b.length] = index;
+    });
+
+	b = b.map(el => {
+        let r = 0;
+        for(let n = number; n!==0;r += n)
+            n = Math.floor(n/el);
+  		return Math.floor(r/a[el]);
+	});
+
+	return Math.min.apply(undefined,b);
 }
